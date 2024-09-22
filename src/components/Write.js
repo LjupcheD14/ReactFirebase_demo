24076dc27bd0923c67e8b7d3701e4ffbf20fb164
Ljubomir <ljupcedavitkov14@gmail.com>
2024-09-22 23:13:57 +0200

    Writing Data

--- src/components/Write.js
+++ src/components/Write.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import app from "../firebaseConf"
+import {getDatabase, ref, set, push} from "firebase/database"
 
 function Write() {
+
+    let [inputValue1, setInputValue1] = useState("");
+    let [inputValue2, setInputValue2] = useState("");
+
+    const saveData = async () => {
+        const db = getDatabase()
+        const newDocRef = push(ref(db, "nature/fruits"));
+        set(newDocRef, {
+            fruitName: inputValue1,
+            fruitDefinition: inputValue2
+        }).then(() => {
+            alert("data saved successfully")
+        }).catch((error) => {
+            alert("error: ", error.message)
+        })
+    }
+
     return (
-        <div>Write</div>
+        <div>
+            <input type="text" value={inputValue1}
+                   onChange={(e) => setInputValue1(e.target.value)}/>
+            <input type="text" value={inputValue2}
+                   onChange={(e) => setInputValue2(e.target.value)}/> <br/>
+
+            <button onClick={saveData}>SAVE DATA</button>
+        </div>
     )
 }
+
 export default Write
